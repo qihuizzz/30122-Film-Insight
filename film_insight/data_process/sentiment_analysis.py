75a27b22c1f02fe4046c2a12c5f3d88cb63cbc2e
ZhenZhenWwvvango0O <zhenzhenwang@uchicago.edu>
2024-03-05 12:29:22 -0600

    sentiment_analysis.py

--- film_insight/data_process/sentiment_analysis.py
+++ film_insight/data_process/sentiment_analysis.py
@@ -1,3 +1,5 @@
+
+Zirong Li & Qihui Zhang
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
